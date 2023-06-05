68a01b6bb8706e5b916ac307a22c16cb436e18ad
Vladislav <pinevichv@gmail.com>
2023-06-05 23:40:48 +0300

    3/4

--- Course works/6 sem/code/src/main.cpp
+++ Course works/6 sem/code/src/main.cpp
@@ -2,20 +2,21 @@
 #include "methods.h"
 
 int main() {
-    std::cout << "ImplicitEuler:\n";
-    ImplicitEuler();
-    std::cout << "\nExplicitEuler:\n";
-    ExplicitEuler();
-    std::cout << "\nSymmetric:\n";
-    Symmetric();
-    std::cout << "\nRungeKutta, 2:\n";
-    RungeKutta2();
-    std::cout << "\nRungeKutta, 4:\n";
-    RungeKutta4();
-    std::cout << "\nexplicit Adams, 4:\n";
-    ExplicitAdams();
-    std::cout << "\nimplicit Adams, 4:\n";
-    ImplicitAdams();
+//    std::cout << "ImplicitEuler:\n";
+//    ImplicitEuler();
+//    std::cout << "\nExplicitEuler:\n";
+//    ExplicitEuler();
+//    std::cout << "\nSymmetric:\n";
+//    Symmetric();
+//    std::cout << "\nRungeKutta, 2:\n";
+//    RungeKutta2();
+//    std::cout << "\nRungeKutta, 4:\n";
+//    RungeKutta4();
+//    std::cout << "\nexplicit Adams, 4:\n";
+//    ExplicitAdams();
+//    std::cout << "\nimplicit Adams, 4:\n";
+//    ImplicitAdams();
+
     std::cout << "\nimplicit Adams, 2:\n";
     Implicit2Adams();
     std::cout << "\nbdf, 2:\n";
